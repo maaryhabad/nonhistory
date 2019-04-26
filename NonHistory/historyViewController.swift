bc0c869f13b732e1d23cfb7e8bb1be8e89fcd9db
Mariana Beilune Abad <maryh.abad@gmail.com>
2019-04-26 11:13:00 -0300

    state machine

--- NonHistory/historyViewController.swift
+++ NonHistory/historyViewController.swift
@@ -9,6 +9,8 @@
 import UIKit
 
 class historyViewController: UIViewController {
+    
+    let hasPassed = stateMachine.shared
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,4 +32,22 @@ class historyViewController: UIViewController {
     override var prefersStatusBarHidden: Bool {
         return true
     }
+    @IBAction func personagemChapter1(_ sender: Any) {
+        if (hasPassed.chapter1) {
+            performSegue(withIdentifier: "chapter1Done", sender: self)
+        } else {
+            hasPassed.chapter1 = true
+            performSegue(withIdentifier: "personagemChapter1", sender: self)
+        }
+    }
+    
+    @IBAction func universoChapter1(_ sender: Any) {
+        if (hasPassed.chapter1) {
+            performSegue(withIdentifier: "chapter1Done", sender: self)
+        } else {
+            hasPassed.chapter1 = true
+            performSegue(withIdentifier: "universoChapter1", sender: self)
+        }
+    }
+    
 }
---
+++ NonHistory/stateMachine.swift
@@ -0,0 +1,22 @@
+//
+//  stateMachine.swift
+//  NonHistory
+//
+//  Created by Mariana Beilune Abad on 26/04/19.
+//  Copyright © 2019 Mariana Beilune Abad. All rights reserved.
+//
+
+import Foundation
+
+class stateMachine {
+    
+    static let shared = stateMachine()
+    
+    var chapter1 = false
+    var chapter2 = false
+    var chapter3 = false
+    var chapter4 = false
+    
+    private init() {}
+    
+}
